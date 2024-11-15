a69ec6c248a2d29d9287030e327f5f2e41717834
ribhavsingla <ribhavsingla2166@gmail.com>
2024-11-15 18:03:30 +0530

    removed unused vars

--- client/src/pages/Contacts.tsx
+++ client/src/pages/Contacts.tsx
@@ -32,7 +32,7 @@ interface CONTACT {
 
 export default function Contacts() {
   const navigate = useNavigate();
-  const [sortBy, setSortBy] = useState<string>('asc');
+  const [sortBy, setSortBy] = useState<string>("asc");
   const [rows, setRows] = useState<CONTACT[]>([]);
   const [totalContacts, setTotalContacts] = useState<number>(200);
   const [page, setPage] = useState<number>(1);
@@ -40,22 +40,25 @@ export default function Contacts() {
   const [refreshContacts, setRefreshContacts] = useState(false);
   const [selectedContact, setSelectedContact] = useState<CONTACT | null>(null);
   const [loading, setLoading] = useState(true);
-  
+
   const handleSortBy = (orderBy: string) => {
     if (sortBy == orderBy) return;
-    else if (sortBy == 'asc') {
-      setSortBy('desc');
+    else if (sortBy == "asc") {
+      setSortBy("desc");
     } else {
-      setSortBy('asc');
+      setSortBy("asc");
     }
   };
 
   useEffect(() => {
     const getContacts = async () => {
       try {
+        setLoading(true)
         const response = await axios.get(
-          `${import.meta.env.VITE_BACKEND_URL}/contacts?page=${page}&sortBy=${sortBy}`
-        );        
+          `${
+            import.meta.env.VITE_BACKEND_URL
+          }/contacts?page=${page}&sortBy=${sortBy}`
+        );
         if (response.status == 200) {
           setTotalContacts(response.data.totalContacts);
           setRows(response.data.contacts);
@@ -69,17 +72,11 @@ export default function Contacts() {
     getContacts();
   }, [refreshContacts, page, sortBy]);
 
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-full">
-        <CircularProgress />
-      </div>
-    );
-  }
+
 
   return (
     <div className="bg-slate-50 py-5">
-      <div className="flex flex-col justify-center items-center gap-5 py-5 px-5 ">
+      <div className="flex flex-col justify-center items-center gap-5 py-5 px-5 h-full">
         <div className="flex justify-start items-center w-full">
           <Button variant="contained" onClick={() => navigate("/")}>
             <ArrowBackIcon />
@@ -88,78 +85,84 @@ export default function Contacts() {
         <h1 className="text-xl font-semibold">SortBy</h1>
         <Stack spacing={2} direction="row">
           <Button
-            variant={sortBy == 'asc' ? `contained` : "outlined"}
-            onClick={() => handleSortBy('asc')}
+            variant={sortBy == "asc" ? `contained` : "outlined"}
+            onClick={() => handleSortBy("asc")}
           >
             Ascending
           </Button>
           <Button
-            variant={sortBy == 'desc' ? `contained` : "outlined"}
-            onClick={() => handleSortBy('desc')}
+            variant={sortBy == "desc" ? `contained` : "outlined"}
+            onClick={() => handleSortBy("desc")}
           >
             Descending
           </Button>
         </Stack>
       </div>
 
-      <div className="px-10">
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>S.No</TableCell>
-                <TableCell align="right">FirstName</TableCell>
-                <TableCell align="right">LastName</TableCell>
-                <TableCell align="right">Email</TableCell>
-                <TableCell align="right">PhoneNumber</TableCell>
-                <TableCell align="right">Company</TableCell>
-                <TableCell align="right">JobTitle</TableCell>
-                <TableCell align="right" colSpan={2}>
-                  Actions
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row: CONTACT, index) => (
-                <TableRow
-                  key={row._id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {LIMIT * (page - 1) + index + 1}
-                  </TableCell>
-                  <TableCell align="right">{row.firstName}</TableCell>
-                  <TableCell align="right">{row.lastName}</TableCell>
-                  <TableCell align="right">{row.email}</TableCell>
-                  <TableCell align="right">{row.phoneNumber}</TableCell>
-                  <TableCell align="right">{row.company}</TableCell>
-                  <TableCell align="right">{row.jobTitle}</TableCell>
-                  <TableCell
-                    align="right"
-                    onClick={() =>
-                      navigate(`/updateContact/${row._id}`, {
-                        state: { contact: row },
-                      })
-                    }
-                  >
-                    <EditNoteIcon className="text-green-600 cursor-pointer" />
-                  </TableCell>
-                  <TableCell
-                    align="right"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      setSelectedContact(row);
-                      setToggleDelete(!toggleDelete);
-                    }}
-                  >
-                    <DeleteIcon className="text-red-600 cursor-pointer" />
+      {loading ? (
+        <div className="flex justify-center items-center h-full">
+          <CircularProgress />
+        </div>
+      ) : (
+        <div className="px-10">
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>S.No</TableCell>
+                  <TableCell align="right">FirstName</TableCell>
+                  <TableCell align="right">LastName</TableCell>
+                  <TableCell align="right">Email</TableCell>
+                  <TableCell align="right">PhoneNumber</TableCell>
+                  <TableCell align="right">Company</TableCell>
+                  <TableCell align="right">JobTitle</TableCell>
+                  <TableCell align="right" colSpan={2}>
+                    Actions
                   </TableCell>
                 </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      </div>
+              </TableHead>
+              <TableBody>
+                {rows.map((row: CONTACT, index) => (
+                  <TableRow
+                    key={row._id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                  >
+                    <TableCell component="th" scope="row">
+                      {LIMIT * (page - 1) + index + 1}
+                    </TableCell>
+                    <TableCell align="right">{row.firstName}</TableCell>
+                    <TableCell align="right">{row.lastName}</TableCell>
+                    <TableCell align="right">{row.email}</TableCell>
+                    <TableCell align="right">{row.phoneNumber}</TableCell>
+                    <TableCell align="right">{row.company}</TableCell>
+                    <TableCell align="right">{row.jobTitle}</TableCell>
+                    <TableCell
+                      align="right"
+                      onClick={() =>
+                        navigate(`/updateContact/${row._id}`, {
+                          state: { contact: row },
+                        })
+                      }
+                    >
+                      <EditNoteIcon className="text-green-600 cursor-pointer" />
+                    </TableCell>
+                    <TableCell
+                      align="right"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        setSelectedContact(row);
+                        setToggleDelete(!toggleDelete);
+                      }}
+                    >
+                      <DeleteIcon className="text-red-600 cursor-pointer" />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      )}
 
       <div className="flex justify-center items-center py-5">
         <Pagination
@@ -168,7 +171,7 @@ export default function Contacts() {
           shape="rounded"
           color="primary"
           page={page}
-          onChange={(event, value) => setPage(value)}
+          onChange={(_, value) => setPage(value)}
         />
       </div>
 
